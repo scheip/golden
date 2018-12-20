ee15a0e721d36e38c97dfbcf05973383fc0ff5ba
scheip <40540800+scheip@users.noreply.github.com>
2018-12-20 07:30:27 -0500

    Create srf2pdf.py
    Push initial version of srf2pdf. First commit to golden repo

---
+++ srf2pdf.py
@@ -0,0 +1,57 @@
+#------------------------------
+# script name: srf2pdf.py
+# project name: golden
+#
+#
+# Author: Corey Scheip, AECOM Technical Services of North Carolina
+#
+# Creation date: 2018/12/18
+#
+# Instructions: Either input a directory as argument to this script or it will default
+# to the directory containing the script (e.g. C:\SRF_Files). When run, a PDf folder will
+# be generated in your working directory (e.g. C:\SRF_Files\pdf and PDF files exported to
+# that location.
+#
+#------------------------------
+
+import win32com.client   # if this fails, you must install module pywin32
+import sys, os
+
+# Create application object
+surf = win32com.client.Dispatch("Surfer.Application")
+surf.Visible = False
+
+# Check for user input directory. If this doesn't exist, default to directory containing script
+if sys.argv[1]:
+    pwd = sys.argv[1]
+else:
+    pwd = os.path.dirname(sys.argv[0])
+
+# Get list of files from working directory
+fileList = os.listdir(pwd)
+
+# Check for PDF directory
+outDir = os.path.join(pwd, 'pdf')
+if not os.path.isdir(outDir):
+    os.mkdir(outDir)
+    print ('PDF Directory created')
+
+# Now go through files, open SRF and print to PDF
+for fil in fileList:
+    print 'Checking file... ' + fil
+    if fil.endswith('.srf'):
+        # Name to use for printing
+        printName = os.path.basename(fil)[:-4]
+
+        # Open surfer file
+        print ' ...opening file...'
+        CurrentDoc = surf.Documents.Open(os.path.join(pwd, fil))
+        pdfFile = os.path.join(outDir, printName + '.pdf')
+        print ' ...printing file...'
+        CurrentDoc.Export2(pdfFile, False, True, "pdfv")
+
+        # Close surfer file
+        CurrentDoc.Close()
+
+# Delete COM object
+del surf
